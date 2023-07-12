d5f58fa6a0c1b679ca4822460ae66edf3bec4031
Osama ayub <ayubosama416@gmail.com>
2023-07-12 18:52:24 +0500

    quiz game Project
    quiz game build using react js, tailwind CSS and redux-toolkit and redux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './pages/MainPage/MainPage';
+
+const App = () => {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+};
+
+export default App;
